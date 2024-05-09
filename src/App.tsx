15b77fd7dfdb34c98a3f4e5847cb18cfa5196ea0
Kyle <45010713+kylekrny@users.noreply.github.com>
2024-05-09 17:00:57 -0600

    update email

--- src/App.tsx
+++ src/App.tsx
@@ -114,7 +114,7 @@ function App() {
               </p>
               <div className='mt-10 flex items-center gap-x-6'>
                 <a
-                  href='mailto:michael@leagueify.org'
+                  href='mailto:hello@leagueify.org'
                   className='rounded-md bg-orange-500 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-700'
                 >
                   Reach out
